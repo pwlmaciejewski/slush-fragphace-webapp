0bdddaecfffe09d2e4bd49f4264ae52ef94629a0
Paweł Maciejewski <pwlmaciejewski@gmail.com>
2016-05-22 15:32:30 +0200

    add missing files

--- templates/webapp/postinstall.sh
+++ templates/webapp/postinstall.sh
@@ -1,5 +1,3 @@
-ln -nfs ../app/models ./node_modules/models
 ln -nfs ../app/routes ./node_modules/routes
-ln -nfs ../app/services ./node_modules/services
 ln -nfs ../app/views ./node_modules/views
 ln -nfs ../app/clients ./node_modules/clients
